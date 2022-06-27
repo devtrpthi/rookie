3fd95f398d50207b31fa6ef076e2cd0ebe971b8b
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-06-27 19:37:53 +0530

    props added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 
 import './App.css';
 
-function Person() {
+function Person(props) {
   return(
     <>
-      <h1>Name:John</h1>
-      <h2>Last Name: Doe</h2>
-      <h3>Age:30</h3>
+      <h1>Name:{props.name}</h1>
+      <h2>Last Name: {props.lastName}</h2>
+      <h3>Age: {props.age}</h3>
     </>
   );
 }
 function App() {
-  const name='John';
-  const isnameshowing=true;
 
   return (
     <div className="App">
-    <Person/>
-      <h1> Hello {isnameshowing ? name:'someone'}! </h1>
+    <Person
+      name='John' lastName='Doe' age={20}
+    />
+    <Person
+      name='Jane' lastName='Wright' age={25}
+    />
     </div>
   );
 }
