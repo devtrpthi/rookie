9d4792fa4c208e9fbb34e3f8a9937b775842b735
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-06-27 20:01:39 +0530

    src cached

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import { useState } from 'react';
-import './App.css';
-
+import React from "react";
 
 function App() {
-  const [count,setCount]=useState(0);
-  
-  return (
-    <div className="App">
-      <button onClick={() => setCount(count-1)}>-</button>
-      <h1>{count} times</h1>
-      <button onClick={() => setCount(count+1)}>+</button>      
-    </div>
-  );
+    return(
+        <>
+            
+        </>
+    );
 }
-
-export default App;
