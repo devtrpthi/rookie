2a69cce9a26a482aef0883014d34474db375464d
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-06-27 19:50:51 +0530

    props added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-
+import { useState } from 'react';
 import './App.css';
 
-function Person(props) {
-  return(
-    <>
-      <h1>Name:{props.name}</h1>
-      <h2>Last Name: {props.lastName}</h2>
-      <h3>Age: {props.age}</h3>
-    </>
-  );
-}
-function App() {
 
+function App() {
+  const [count,setCount]=useState(0);
+  
   return (
     <div className="App">
-    <Person
-      name='John' lastName='Doe' age={20}
-    />
-    <Person
-      name='Jane' lastName='Wright' age={25}
-    />
+      <button onClick={() => setCount(count-1)}>-</button>
+      <h1>{count} times</h1>
+      <button onClick={() => setCount(count+1)}>+</button>      
     </div>
   );
 }
