bfa60ef14e050388de9887171f8fc0fd08051655
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-06-27 20:08:35 +0530

    react-app

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 
 function App() {
     return(
-        <>
-            
-        </>
+        <div>
+        <h1> Hii Boiizz!</h1>
+        </div>
     );
 }
+
+export default App;
