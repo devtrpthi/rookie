cb546d18b50c8d024c2251c7d3a6b0e48ce2fd57
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-06-27 18:41:19 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 
 import './App.css';
 
+function Person() {
+  return(
+    <>
+      <h1>Name:John</h1>
+      <h2>Last Name: Doe</h2>
+      <h3>Age:30</h3>
+    </>
+  );
+}
 function App() {
   const name='John';
   const isnameshowing=true;
-  
+
   return (
     <div className="App">
+    <Person/>
       <h1> Hello {isnameshowing ? name:'someone'}! </h1>
     </div>
   );
