8544823e0a9c573132d8c1c2672b99159025587b
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-06-28 11:43:18 +0530

    app.js modified

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import {React, useEffect} from "react";
 import './App.css';
-
+import SearchIcon from './search.svg';
+import MovieCard from "./Moviecard";
 
 const URL = 'http://www.omdbapi.com?apikey=61b71d05 ';
+
+const movie1 = {
+    "Title": "Amazing Spiderman Syndrome",
+    "Year": "2012",
+    "imdbID": "tt2586634",
+    "Type": "movie",
+    "Poster": "N/A"
+}
 
 function App() {
 
@@ -16,8 +25,23 @@ function App() {
         searchmovies('Spiderman');
     }, [])
     return(
-        <div>
-        <h1> Hii Boiizz!</h1>
+        <div className="app">
+        <h1>MovieLand</h1>
+
+        <div className="search">
+            <input
+            placeholder="Search for movies"
+            value="Superman"
+            onChange={() => {}} />
+            <img
+            src={SearchIcon}
+            alt="search"
+            onClick={() => {}} />
+        </div>
+
+            <div className="container">
+                <MovieCard movie1={movie1}/>
+            </div>
         </div>
     );
 }
