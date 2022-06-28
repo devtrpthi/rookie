0cfe824ec8efb6c1010c1d1d4a7e62e967937c4b
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-06-28 10:09:30 +0530

    fetch data using api key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from "react";
+import {React, useEffect} from "react";
+
+
+
+const URL = 'http://www.omdbapi.com?apikey=61b71d05 ';
 
 function App() {
+
+    const searchmovies = async (title) =>{
+        const response = await fetch(`${URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+    useEffect(() => {
+        searchmovies('Spiderman');
+    }, [])
     return(
         <div>
         <h1> Hii Boiizz!</h1>
